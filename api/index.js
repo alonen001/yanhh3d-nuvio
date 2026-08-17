97ed6161e192d85f33624620f7727768ba63d0fe
alonen001 <tao2tk@gmail.com>
2026-08-17 13:34:32 +0700

    Add Vercel serverless entrypoint

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const { getRouter } = require('stremio-addon-sdk');
+const addonInterface = require('../addon');
+
+const router = getRouter(addonInterface);
+
+module.exports = (req, res) => router(req, res);
